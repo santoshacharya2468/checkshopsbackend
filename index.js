29035864365a5c318c86db68c3b44de5c9c59027
santosh devkota <devkota549@gmail.com>
2020-06-12 17:41:31 +0545

    no edits

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 //middleware
-const appMiddleware=require("./middlewares/appmiddleware");
+//const appMiddleware=require("./middlewares/appmiddleware");
 //models
-const User=require("./models/user");
+const User = require("./models/user");
 dotenv.config();
 const app = express();
 const port = 8080;
 mongoose.connect(process.env.dbCon, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex:true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
 });
-app.listen(process.env.port,"192.168.1.153",() => console.log(`Server running on port ..${process.env.port}`));
+app.listen(process.env.port, "192.168.100.101", () =>
+  console.log(`Server running on port ..${process.env.port}`)
+);
 app.use(express.json());
-app.use(appMiddleware);
+//app.use(appMiddleware);
 //routes
-const accountRoute=require("./routes/accountRoute");
-const shopRoute=require("./routes/shopRoute");
-app.use("/account",accountRoute);
-app.use("/shop",shopRoute);
+const accountRoute = require("./routes/accountRoute");
+const shopRoute = require("./routes/shopRoute");
+app.use("/account", accountRoute);
+app.use("/shop", shopRoute);
