7fb08e8acccf98ae67f6bb638e2dc626de41995c
Santosh Acharya <santoshacharya2468@gmail.com>
2020-06-16 13:13:31 +0545

    'added-click'

---
+++ routes/clickRoute.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const authorization=require("../middlewares/authorization");
+const Click=require("../models/click");
+const Shop=require("../models/shop");
+router.post("/:shopId",async(req,res)=>{
+    try{
+        var click=new Click({
+            shop:req.params.shopId
+        });
+        var result=await click.save();
+        res.status(200).send({message:"Added to click"});
+    }
+    catch(e){
+        res.status(500).send();
+    }
+
+});
+router.get("/",authorization,async(req,res)=>{
+    try{
+        var shop=await Shop.find({owner:req.user});
+        var result=await click.find({shop:shop});
+        res.send(result);
+    }
+    catch(e){
+        res.status(500).send();
+    }
+
+});
